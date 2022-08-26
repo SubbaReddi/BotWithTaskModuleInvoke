c77cb97ed5c235d7b4874fd929763de95d1ab4af
Subba Reddi Tummuru (WIPRO LIMITED) <v-sutumm@microsoft.com>
2022-08-25 23:17:37 -0600

    Second commit

---
+++ 07.using-adaptive-cards/Bots/AdaptiveCardsBot.cs
@@ -0,0 +1,132 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
+using AdaptiveCards;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Builder.Teams;
+using Microsoft.Bot.Schema;
+using Microsoft.Bot.Schema.Teams;
+using Microsoft.Recognizers.Definitions;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json.Schema;
+
+namespace AdaptiveCardsBot
+{
+    // This bot will respond to the user's input with an Adaptive Card.
+    // Adaptive Cards are a way for developers to exchange card content
+    // in a common and consistent way. A simple open card format enables
+    // an ecosystem of shared tooling, seamless integration between apps,
+    // and native cross-platform performance on any device.
+    // For each user interaction, an instance of this class is created and the OnTurnAsync method is called.
+    // This is a Transient lifetime service. Transient lifetime services are created
+    // each time they're requested. For each Activity received, a new instance of this
+    // class is created. Objects that are expensive to construct, or have a lifetime
+    // beyond the single turn, should be carefully managed.
+
+    public class AdaptiveCardsBot : TeamsActivityHandler
+    {
+        private const string WelcomeText = @"This bot will introduce you to AdaptiveCards.
+                                            Type anything to see an AdaptiveCard.";
+
+        // This array contains the file location of our adaptive cards
+        private readonly string[] _cards =
+        {
+            Path.Combine(".", "Resources", "Adaptivecard1.json")
+        };
+
+        protected override Task OnConversationUpdateActivityAsync(ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            return base.OnConversationUpdateActivityAsync(turnContext, cancellationToken);
+        }
+
+        protected override Task<TaskModuleResponse> OnTeamsTaskModuleFetchAsync(ITurnContext<IInvokeActivity> turnContext, TaskModuleRequest taskModuleRequest, CancellationToken cancellationToken)
+        {
+            var asJobject = JObject.FromObject(taskModuleRequest.Data);
+
+            var taskInfo = new TaskModuleTaskInfo();
+            taskInfo.Url = taskInfo.FallbackUrl = "https://29c9-2604-3d09-2881-8f00-d00e-adb3-fd50-b773.ngrok.io"; // Task module URL to open
+            SetTaskInfo(taskInfo);
+
+            return Task.FromResult( new TaskModuleResponse
+            {
+                Task = new TaskModuleContinueResponse()
+                {
+                    Value = taskInfo,
+                },
+            });
+        }
+
+        private static void SetTaskInfo(TaskModuleTaskInfo taskInfo)
+        {
+            taskInfo.Height = 1000;
+            taskInfo.Width = 700;
+            taskInfo.Title = "Task Module Title";
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            await SendWelcomeMessageAsync(turnContext, cancellationToken);
+        }
+
+        protected override async Task OnInstallationUpdateActivityAsync(ITurnContext<IInstallationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var activity = turnContext.Activity; if
+            (string.Equals(activity.Action, "Add",
+            StringComparison.InvariantCultureIgnoreCase))
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Welcome message"), cancellationToken);
+            }
+            else
+            {
+                await turnContext.SendActivityAsync(MessageFactory.Text($"Exit message"), cancellationToken);
+            }
+            return;
+        }
+
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            
+
+            var value = turnContext.Activity.From.Id;
+
+            var r = new Random();
+            var cardAttachment = CreateAdaptiveCardAttachment(_cards[0]);
+
+            var welcomeAttachments = new List<Attachment>();
+            welcomeAttachments.Add(cardAttachment);
+
+            await turnContext.SendActivityAsync(MessageFactory.Attachment(cardAttachment), cancellationToken);
+            await turnContext.SendActivityAsync(MessageFactory.Text("Please enter any text to see another card."), cancellationToken);
+        }
+
+        private static async Task SendWelcomeMessageAsync(ITurnContext turnContext, CancellationToken cancellationToken)
+        {
+            foreach (var member in turnContext.Activity.MembersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(
+                        $"Welcome to Adaptive Cards Bot {member.Name}. {WelcomeText}",
+                        cancellationToken: cancellationToken);
+                }
+            }
+        }
+
+        private static Attachment CreateAdaptiveCardAttachment(string filePath)
+        {
+            var adaptiveCardJson = File.ReadAllText(filePath);
+            var adaptiveCardAttachment = new Attachment()
+            {
+                ContentType = "application/vnd.microsoft.card.adaptive",
+                Content = JsonConvert.DeserializeObject(adaptiveCardJson),
+            };
+            return adaptiveCardAttachment;
+        }
+    }
+}
